82e351910f8bf82d547a501f61f857db71763c4c
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-11-09 10:21:18 -0500

    cleanup before merge

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,20 +1,31 @@
 import React from "react";
-import shutterstock from "../images/elder.png";
-// import h2Generator from "../images/generator.png"
-import childMask from "../images/Child in mask.jpg"
-import device from "../images/IMG_6101.jpg"
+import elder from "../images/elder.png";
 
 const HomePage = (props) => {
   return (
     <div>
-      <h1 className="" style={{textAlign: "center"}}>Better Recovery from Stroke and Cardiac Ischemic Events</h1>
+      {/* <h1 className="" style={{ textAlign: "center" }}>
+        Better Recovery from Stroke and Cardiac Ischemic Events
+      </h1> */}
       <div className="cover-container">
-        <img className="cover-photo" src={shutterstock} alt="https://www.shutterstock.com/image-photo/adult-granddaguhter-supporting-her-senior-grandmother-2172757295"/>
+        <img
+          className="cover-photo"
+          src={elder}
+          alt="https://www.shutterstock.com/image-photo/adult-granddaguhter-supporting-her-senior-grandmother-2172757295"
+        />
       </div>
-      <div>
-        <h1>Our Mission</h1>
-        <p>Create the first officially-approved inhaled hydrogen therapy system, and improve the recovery of millions of the millions of people impacted by stroke and cardiac arrest</p>
-        <p>HydroSpire is a research-stage company that currently has no approved products available for human use </p>
+      <div className="mission-container">
+        <h1 style={{ fontSize: "80px" }}>Our Mission</h1>
+        <div className="hl"></div>
+        <h3>
+          Create the first officially-approved inhaled hydrogen therapy system, and improve the
+          recovery of millions of the millions of people impacted by stroke and cardiac arrest
+        </h3>
+        <p>
+          HydroSpire is a research-stage company that currently has no approved products available
+          for human use{" "}
+        </p>
+        <div className="home-page-item"></div>
       </div>
     </div>
   );
--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,26 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignOutButton from "../authentication/SignOutButton";
 import logo from "../../images/Hydrospire logo large.jpg";
 
 const TopBar = ({ user }) => {
-  const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link to="/user-sessions/new">Sign In</Link>
-    </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="button">
-        Sign Up
-      </Link>
-    </li>,
-  ];
-
-  const authenticatedListItems = [
-    <li key="sign-out">
-      <SignOutButton />
-    </li>,
-  ];
-
   return (
     <div className="top-bar">
       <div className="top-bar-left">
@@ -28,7 +10,6 @@ const TopBar = ({ user }) => {
           <Link to="/">
             <img className="home-page-logo" src={logo} style={{ height: "4rem" }} />
           </Link>
-          {/* <li className="menu-text">App</li> */}
         </ul>
       </div>
       <div className="top-bar-right">
